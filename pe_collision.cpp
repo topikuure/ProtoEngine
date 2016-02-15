59809b14db7ff5d7085b36e896f93d03a63632de
topikuure <topikuure@users.noreply.github.com>
2016-02-15 19:51:24 +0200

    Paranneltiin Collision::handle -metodia
    BoundingBoxien kulmissa tapahtuva törmäys on nyt sulavaa

--- pe_collision.cpp
+++ pe_collision.cpp
@@ -14,14 +14,9 @@ void Collision::handle(BoundingBox &b1, const BoundingBox &b2)
     {
     Vector2D direction = Vector2D(b2.x - b1.x, b2.y - b1.y);
     direction.normalize();
-    if(direction.x > 0.777) b1.x--;
-    else if(direction.x < -0.777) b1.x++;
-    else if(direction.y > 0.777) b1.y--;
-    else if(direction.y < -0.777) b1.y++;
-    else
-        {
-        b1.x -= direction.x;
-        b1.y -= direction.y;
-        }
+    if(direction.x >= 0.7) b1.x--;
+    else if(direction.x <= -0.7) b1.x++;
+    else if(direction.y >= 0.7) b1.y--;
+    else if(direction.y <= -0.7) b1.y++;
     }
 }
